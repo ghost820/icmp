f3871ce4619b9d8bf056c988b53f2bdf43e4a23b
ghost <ghost@unknown.com>
2024-10-03 12:36:50 +0200

    Parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use pnet::packet::icmp::{echo_reply, echo_request, IcmpPacket, IcmpTypes};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::Packet;
 use socket2::{Domain, Protocol, Socket, Type};
 use std::io::{self, Error};
 use std::mem::MaybeUninit;
@@ -9,7 +12,32 @@ fn main() -> io::Result<()> {
     loop {
         match socket.recv_from(&mut buf) {
             Ok((n, src_addr)) => {
-                println!("{}", n);
+                let buf_ptr = buf.as_ptr() as *const u8;
+                let packet_slice = unsafe { std::slice::from_raw_parts(buf_ptr, n) };
+
+                if let Some(ipv4_packet) = Ipv4Packet::new(packet_slice) {
+                    let ipv4_payload = ipv4_packet.payload();
+
+                    if let Some(icmp_packet) = IcmpPacket::new(ipv4_payload) {
+                        match icmp_packet.get_icmp_type() {
+                            IcmpTypes::EchoRequest => {
+                                if let Some(echo_request) =
+                                    echo_request::EchoRequestPacket::new(icmp_packet.packet())
+                                {
+                                    println!("[Recv] Echo Request");
+                                }
+                            }
+                            IcmpTypes::EchoReply => {
+                                if let Some(echo_reply) =
+                                    echo_reply::EchoReplyPacket::new(icmp_packet.packet())
+                                {
+                                    println!("[Recv] Echo Reply");
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                }
             }
             Err(e) => {
                 return Err(Error::new(io::ErrorKind::Other, format!("{}", e)));
